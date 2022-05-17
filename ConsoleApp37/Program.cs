f843508c48eb92a4a76790f0e5075a448e1b48fb
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-05-17 16:52:01 +0300

    encapsulation example

--- ConsoleApp37/Program.cs
+++ ConsoleApp37/Program.cs
@@ -2,49 +2,41 @@
 
 namespace ConsoleApp37
 {
-    class Student
+    class Gun
     {
-        public Guid id;
-        public string firstName;
-        public string lastName;
-        public string middleName;
-        public int age;
-        public string group;
+        private bool isLoaded;
 
+        private void Reload()
+        {
+            Console.WriteLine("Заряжаю..");
+
+            isLoaded = true;
+
+            Console.WriteLine("Заряжено!");
+        }
+
+        public void Shoot()
+        {
+            if (!isLoaded)
+            {
+                Console.WriteLine("Оружиме не заряжено");
+                Reload();
+
+            }
+            Console.WriteLine("ТЫЩ! ТЫЩ!\n");
+            isLoaded = false;
+        }
     }
 
     internal class Program
     {
 
-        static Student GetStudent()
-        {
-            Student student = new Student();
-
-            student.firstName = "Мартин";
-            student.lastName = "Курилка";
-            student.middleName = "Пупик";
-            student.id = Guid.NewGuid();
-            student.age = 10;
-            student.group = "ЦУФ-113";
-
-            return student;
-        }
-        static void Print(Student student)
-        {
-            Console.WriteLine("Информация о студенте!");
-            Console.WriteLine();
-            Console.WriteLine($"Имя: {student.firstName}");
-            Console.WriteLine($"Отчество: {student.lastName}");
-            Console.WriteLine($"Фамилия: {student.middleName}");
-            Console.WriteLine($"Возраст: {student.age}");
-            Console.WriteLine($"Группа: {student.group}");
-        }
 
         static void Main(string[] args)
         {
-            var firstStudent = GetStudent();
+            Gun gun = new Gun();
 
-            Print(firstStudent);
+
         }
     }
 }
