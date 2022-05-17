a5c674c3f8baab4e4ae71379200fc384b5b9aad8
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-05-17 13:50:12 +0300

    ООП C#

--- ConsoleApp37/Program.cs
+++ ConsoleApp37/Program.cs
@@ -2,14 +2,49 @@
 
 namespace ConsoleApp37
 {
+    class Student
+    {
+        public Guid id;
+        public string firstName;
+        public string lastName;
+        public string middleName;
+        public int age;
+        public string group;
+
+    }
+
     internal class Program
     {
 
+        static Student GetStudent()
+        {
+            Student student = new Student();
 
+            student.firstName = "Мартин";
+            student.lastName = "Курилка";
+            student.middleName = "Пупик";
+            student.id = Guid.NewGuid();
+            student.age = 10;
+            student.group = "ЦУФ-113";
+
+            return student;
+        }
+        static void Print(Student student)
+        {
+            Console.WriteLine("Информация о студенте!");
+            Console.WriteLine();
+            Console.WriteLine($"Имя: {student.firstName}");
+            Console.WriteLine($"Отчество: {student.lastName}");
+            Console.WriteLine($"Фамилия: {student.middleName}");
+            Console.WriteLine($"Возраст: {student.age}");
+            Console.WriteLine($"Группа: {student.group}");
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var firstStudent = GetStudent();
+
+            Print(firstStudent);
         }
     }
 }
