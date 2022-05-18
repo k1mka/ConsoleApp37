4dbd3c4bfe3b096b47594479e40726a2d9a810d8
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-05-18 19:10:04 +0300

    passed another constructor to the class object

--- ConsoleApp37/Program.cs
+++ ConsoleApp37/Program.cs
@@ -37,7 +37,7 @@ namespace ConsoleApp37
 
         static void Main(string[] args)
         {
-            Student student1 = new Student("Артем", new DateTime(2000, 02, 12));
+            Student student1 = new Student("Артем", "Романенко", "Романович", new DateTime(2000, 02, 12));
             student1.Print();
         }
     }
